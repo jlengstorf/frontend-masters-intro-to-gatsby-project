fe3d9972d35440a41f2d9626b0f39c5bc17595a9
Jason Lengstorf <jason@lengstorf.com>
2021-08-08 13:06:06 -0700

    wip: maybe ready

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,34 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Seo } from '../components/seo.js';
 import { header, content } from '../styles/layout.module.css';
 
 import '../styles/global.css';
 
-export function Layout({ children }) {
+export default function Layout({
+  children,
+  title = false,
+  description = false,
+  image = false,
+  path = false,
+}) {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const meta = data?.site?.siteMetadata ?? {};
+
   return (
     <>
+      <Seo title={title} description={description} image={image} path={path} />
       <header className={header}>
-        <Link to="/">Intro to Gatsby</Link>
+        <Link to="/">{meta.title}</Link>
         <nav>
           <Link to="/about">About</Link>
         </nav>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
-import { Layout } from '../components/layout';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../components/layout';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
-    <Layout>
+    <Layout title="About This Site">
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate floral arrangement with dry ice mist curling out and around it"
+      />
       <h1>About This Site</h1>
       <Link to="/">Back to home</Link>
     </Layout>
