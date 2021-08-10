ded7d6671981d504d647a69b0a741e6c235e652b
Jason Lengstorf <jason@lengstorf.com>
2021-08-10 18:54:59 -0500

    first three steps; should have been committing

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About This Site</h1>
+      <Link to="/">Back to home</Link>
+    </main>
+  );
+}
