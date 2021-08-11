bd8ae12728d594845f7a1084ab4d8608c36d2fd7
Adrian Ross <adriantoddross@gmail.com>
2021-08-11 16:04:14 -0400

    Stuck

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,26 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
-
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
-    <Layout>
-      <h1>About this site</h1>
+    <Layout title="About This Site">
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate floral arrangement with dry ice mist curling out and around it"
+      />
+      <h1>About This Site</h1>
       <Link to="/">Back to home</Link>
     </Layout>
   );
