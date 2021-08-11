27f6d9370c461fc8fa975e04fda87fa571e17f5a
Jason Lengstorf <jason@lengstorf.com>
2021-08-11 14:47:39 -0500

    add static images and MDX static images

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,32 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    'gatsby-remark-images',
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
         defaultLayouts: {
           posts: require.resolve('./src/components/post-layout.js'),
         },
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
