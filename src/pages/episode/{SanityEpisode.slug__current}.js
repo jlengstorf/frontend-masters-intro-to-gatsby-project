10417ccb392b6661c879efa7916b6f4bccdea05b
Jason Lengstorf <jason@lengstorf.com>
2021-08-11 15:45:37 -0500

    handle missing data

--- src/pages/episode/{SanityEpisode.slug__current}.js
+++ src/pages/episode/{SanityEpisode.slug__current}.js
@@ -27,10 +27,12 @@ export default function SanityEpisode({ data }) {
 
   return (
     <Layout title={episode.title} description={episode.description}>
-      <GatsbyImage
-        image={episode.image.asset.gatsbyImageData}
-        alt={episode.title}
-      />
+      {episode.image?.asset?.gatsbyImageData && (
+        <GatsbyImage
+          image={episode.image.asset.gatsbyImageData}
+          alt={episode.title}
+        />
+      )}
       <h1>{episode.title}</h1>
       <p>
         (posted {episode.date}) — {episode.description}
