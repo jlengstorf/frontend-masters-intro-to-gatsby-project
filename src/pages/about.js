b8f0b7bdae2cac049c35e0651e81dfde0171a746
Adrian Ross <adriantoddross@gmail.com>
2021-08-11 11:33:00 -0400

    added links between pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function AboutPage() {
   return (
     <main>
       <h1>About this site</h1>
+      <Link to="/">Back to home</Link>
     </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Hello Frontend Masters!</h1>
+      <Link to="/about">About this site</Link>
     </main>
   );
 }
