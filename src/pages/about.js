b31fcd0286ae76048c75895dd402b252cfaa793e
Jason Lengstorf <jason@lengstorf.com>
2021-08-11 11:36:27 -0500

    add and use a shared layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-import { Seo } from '../components/seo.js';
+import Layout from '../components/layout.js';
 
 export default function AboutPage() {
   return (
-    <>
-      <Seo
-        title="About This Site"
-        description="More information about this site."
-      />
-      <main>
-        <h1>About This Site</h1>
-        <Link to="/">Back to home</Link>
-      </main>
-    </>
+    <Layout
+      title="About This Site"
+      description="More information about this site."
+    >
+      <h1>About This Site</h1>
+      <Link to="/">Back to home</Link>
+    </Layout>
   );
 }
