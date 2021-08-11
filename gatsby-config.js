88c8337b9f8e3761e0e20e69a02b655115e24c19
Jason Lengstorf <jason@lengstorf.com>
2021-08-11 11:26:38 -0500

    add metadata, add SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,5 @@ module.exports = {
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
   },
+  plugins: ['gatsby-plugin-react-helmet'],
 };
