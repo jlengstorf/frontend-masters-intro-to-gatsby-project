d66a18205b70fc6720b00e442b32d758848b6c9d
Adrian Ross <adriantoddross@gmail.com>
2021-08-11 12:44:59 -0400

    Use Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,13 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 
-import { Seo } from '../components/seo';
+import Layout from '../components/layout';
 
 export default function AboutPage() {
   return (
-    <>
-      <Seo
-        title="About this site"
-        description="More information about this site"
-      />
-      <main>
-        <h1>About this site</h1>
-        <Link to="/">Back to home</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>About this site</h1>
+      <Link to="/">Back to home</Link>
+    </Layout>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,13 @@
 import * as React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Link } from "gatsby";
 
-import { Seo } from '../components/seo';
+import Layout from '../components/layout';
 
 export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  const meta = data?.site?.siteMetadata ?? {};
-
   return (
-    <>
-      <Seo />
-      <header>
-        <Link to="/">{meta.title}</Link>
-      </header>
-      <main>
-        <h1>Hello Frontend Masters!</h1>
-        <Link to="/about">About this site</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>Hello Frontend Masters!</h1>
+      <Link to="/about">About this site</Link>+{' '}
+    </Layout>
   );
 }
