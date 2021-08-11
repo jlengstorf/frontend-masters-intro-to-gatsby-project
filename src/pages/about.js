4fa17e0005769734828c2025c81c4988a64b1f30
Jason Lengstorf <jason@lengstorf.com>
2021-08-10 21:58:50 -0500

    query images using GraphQL

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,28 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout.js';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
     <Layout
       title="About This Site"
       description="More information about this site."
     >
-      <main></main>
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate floral arrangement with dry ice mist curling out and around it"
+      />
       <h1>About This Site</h1>
       <Link to="/">Back to home</Link>
     </Layout>
