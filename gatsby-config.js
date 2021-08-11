4e875cf6540bd708cc74e898b684489673958239
Jason Lengstorf <jason@lengstorf.com>
2021-08-10 21:37:10 -0500

    add static images

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,15 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout.js';
+
+import { imageWrapper } from '../styles/index.module.css';
 
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
@@ -23,6 +26,16 @@ export default function IndexPage() {
 
   return (
     <Layout>
+      <div className={imageWrapper}>
+        <StaticImage
+          src="../images/ivana-la-61jg6zviI7I-unsplash.jpg"
+          alt="a corgi sitting on a bed with red paper hearts all over it. it looks unamused."
+          placeholder="dominantColor"
+          width={300}
+          height={300}
+        />
+      </div>
+
       <h1>Hello Frontend Masters!</h1>
       <Link to="/about">About this site</Link>
 
