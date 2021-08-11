51a3ee8b7f3785b142427e8ee14b880fc047167b
Adrian Ross <adriantoddross@gmail.com>
2021-08-11 11:26:53 -0400

    Created home & about page

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About this site</h1>
+    </main>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello Frontend Masters!</h1>
+    </main>
+  );
+}
