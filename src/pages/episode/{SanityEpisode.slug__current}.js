2b2b2761f9c4a03b5f26e126bc060967e8d01128
Jason Lengstorf <jason@lengstorf.com>
2021-08-10 22:12:38 -0500

    generate pages from Sanity data

---
+++ src/pages/episode/{SanityEpisode.slug__current}.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import Layout from '../../components/layout.js';
+
+export const query = graphql`
+  query SanityEpisode($id: String!) {
+    sanityEpisode(id: { eq: $id }) {
+      title
+      description
+      slug {
+        current
+      }
+      youtubeID
+      date(fromNow: true)
+      image {
+        asset {
+          gatsbyImageData(placeholder: DOMINANT_COLOR)
+        }
+      }
+    }
+  }
+`;
+
+export default function SanityEpisode({ data }) {
+  const episode = data.sanityEpisode;
+
+  return (
+    episode.youtubeID && (
+      <Layout title={episode.title} description={episode.description}>
+        {episode.image ? (
+          <GatsbyImage
+            image={episode.image.asset.gatsbyImageData}
+            alt={episode.title}
+          />
+        ) : null}
+        <h1>{episode.title}</h1>
+        <p>
+          (posted {episode.date}) — {episode.description}
+        </p>
+        <ul>
+          <li>
+            <a href={`https://www.learnwithjason.dev/${episode.slug.current}`}>
+              full episode and details
+            </a>
+          </li>
+          <li>
+            <a href={`https://youtu.be/${episode.youtubeID}`}>
+              watch on YouTube
+            </a>
+          </li>
+        </ul>
+      </Layout>
+    )
+  );
+}
