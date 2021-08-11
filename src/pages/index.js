c25a0c5a556ac1a4e7982b26318a9e2d8a11c19a
Jason Lengstorf <jason@lengstorf.com>
2021-08-10 21:22:39 -0500

    show blog posts on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,40 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout.js';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            description
+            date(fromNow: true)
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
       <h1>Hello Frontend Masters!</h1>
       <Link to="/about">About this site</Link>
+
+      <h2>Check out my recent blog posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+            <small>posted {post.frontmatter.date}</small>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
