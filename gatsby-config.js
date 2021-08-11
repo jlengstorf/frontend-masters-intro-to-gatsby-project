30e3b1188445123e2dbfc8b2121ede809608582b
Adrian Ross <adriantoddross@gmail.com>
2021-08-11 14:30:49 -0400

    Add plugins to enable MDX blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,30 @@ module.exports = {
     image:
       'https://res.cloudinary.com/adriantoddross/image/upload/v1596736103/Adrian_Ross_-_Headshot_3.jpg',
   },
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+
+    // This set of plug-ins is to enable creating MDX blog posts from the ./src/ folder
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
