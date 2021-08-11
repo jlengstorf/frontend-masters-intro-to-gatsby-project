a30b4e3f33c91873e2923f1ccf49bb259635cbbb
Adrian Ross <adriantoddross@gmail.com>
2021-08-11 11:49:16 -0400

    Make my first GraphQL query to grab metadata

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.yourdomain.tld',
+    title: '🍕🍗🥦 Frontend Masters Intro to Gatsby',
+    description: 'Frontend Masters Intro to Gatsby Course',
+    image:
+      'https://res.cloudinary.com/adriantoddross/image/upload/v1596736103/Adrian_Ross_-_Headshot_3.jpg',
+  },
+};
