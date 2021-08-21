00b23f11c0b07d1af1cc102fa5bcacb72a9427db
Rayen Mehrez <54637377+rayenmehrez77@users.noreply.github.com>
2021-08-21 15:49:18 +0200

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Frontend Masters Intro to Gatsby',
+    description: 'Frontend Masters Intro to Gatsby course project',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+};
