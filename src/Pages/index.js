8de684f8fd49dea75b9287ae684dd04eb328165d
Rayen Mehrez <54637377+rayenmehrez77@users.noreply.github.com>
2021-08-21 15:18:38 +0200

    add links

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Hello Front end Masters!</h1>
+      <Link to="/about">Go to about</Link>
     </main>
   );
 }
