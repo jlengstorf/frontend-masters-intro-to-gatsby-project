c07a89e160d3101f5234ad7ab8b13b9a2a7b6a31
Tyler Weber <tweber@xevo.com>
2021-10-21 16:58:13 -0700

    Added css with module

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout.js';
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>Hello Frontend Masters</h1>
+      <h1>Hello Frontend Masters!</h1>
       <Link to="/about">About this website</Link>
     </Layout>
   );
