f745674f74c54e493d8ba3babd8ed7aeeb0b24a6
Tyler Weber <tweber@xevo.com>
2021-10-21 16:28:13 -0700

    Added layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout.js';
 
 export default function AboutPage() {
   return (
-    <main>
+    <Layout
+      title="About This Site"
+      description="More information about this site"
+    >
       <h1>About This Site</h1>
       <Link to="/">Back to home</Link>
-    </main>
+    </Layout>
   );
 }
