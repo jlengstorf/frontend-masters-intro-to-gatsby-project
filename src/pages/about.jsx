ef41dbcbf0af33ccd1db2e5120bd9054304cfaaa
Roy Tanaka <tanaka@mac.com>
2021-12-13 20:01:17 -0500

    adds pages

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>Hello from about</h1>
+    </main>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Hello frontend masters</h1>
+    </main>
+  )
+}
+
+export default IndexPage
