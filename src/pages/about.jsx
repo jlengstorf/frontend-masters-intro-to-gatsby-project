fa451ef1a11cc1bb9126aed4a42c241ae65aace9
Roy Tanaka <tanaka@mac.com>
2021-12-13 20:06:24 -0500

    adds links

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 const AboutPage = () => {
   return (
     <main>
       <h1>Hello from about</h1>
+      <Link to="/">Back to home</Link>
     </main>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 const IndexPage = () => {
   return (
     <main>
       <h1>Hello frontend masters</h1>
+      <Link to="/about">Link to about</Link>
     </main>
   )
 }
