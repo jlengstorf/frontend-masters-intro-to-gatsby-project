81ee4f45c8c40dc6e095540116aef4d6c79e150d
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2023-09-04 16:55:12 +0300

    chore:dd

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
 
-import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../componet/layout';
 
 export default function IndexPage() {
   return (
     <Layout>
       <h1>Hhhhh</h1>
-      <Link to="/about">About this site</Link>
     </Layout>
   );
 }
--- src/componet/layout.js
+++ src/componet/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-
+import '../styles/global.css';
 export default function Layout({
   children,
   title = false,
